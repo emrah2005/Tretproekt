14a0137313db1aa93a5997804f8976f054ec298b
emrah2005 <92210556+emrah2005@users.noreply.github.com>
2025-11-23 21:18:37 +0100

    Create 2024_11_23_000009_create_threads_table.php

---
+++ database/migrations/2024_11_23_000009_create_threads_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('contract_id');
+            $table->string('subject');
+            $table->unsignedBigInteger('creator_id');
+            $table->boolean('is_resolved')->default(false);
+            $table->timestamps();
+            
+            $table->foreign('contract_id')->references('id')->on('contracts')->onDelete('cascade');
+            $table->foreign('creator_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('threads');
+    }
+};
